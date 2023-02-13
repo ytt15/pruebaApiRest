cda009bee99fd82a3ec74ab2a5c28042e1a8deb9
ytt15 <101960564+ytt15@users.noreply.github.com>
2023-02-12 21:04:51 -0500

    Prueba Api Rest Terminada

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,8 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'db_links'
+        database: 'db_links',
+        port: '3306'
     }
 
 };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -45,3 +45,4 @@ router.get('/profile', isLoggedIn, (req, res) => {
 });
 
 module.exports = router;
+
